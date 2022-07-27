8290cd6bde1785a618f737c33e7e6fc768c165ba
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-27 12:51:23 -0300

    blast off in 10 seconds

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('hi');
+function countDown(){
+
+
+
+
+
+
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+ let i = 10;
+
+ while(i>=0){
+
+    const para = document.createElement('p');
+    
+    if(i ===10){
+        para.textContent = 'Starting countdown in \n 10';
+    }
+    else if(i === 0){
+        para.textContent = "🎆🎇✨🎇💥💥💥 Blast off!! 🎆🎇✨🎇💥💥💥";
+    }else{
+        para.textContent= `>>>>>>>: ${i}`;
+    }
+output.appendChild(para);
+i--;
+
+ }
+// 
+// para.textContent = ;
+// output.appendChild(para);
+
+
+ 
+
+
+
+
+}
+
+let btn = document.querySelector('.button');
+ btn.onclick= countDown;
