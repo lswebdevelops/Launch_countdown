88f2bd3a9fa0d61d638a3050643f1dc57f407600
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-28 11:03:38 -0300

    blast off writen using while loop

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ output.innerHTML = '';
 let i = 10;
 //inside the loop:
 
+  
+while (i >=0){    
 
+    const para = document.createElement('p');
 
-for(let i = 10; i>= 0; i--){
-      
-    
     if(i ===10){
-        const para = document.createElement('p');
+                
     para.textContent = `Countdown starting!`;
-    output.appendChild(para);
+   
     }
      else if( i=== 0){
-        const para = document.createElement('p');
+       
         para.textContent = `💥💥Blast Off!!💥💥`;
-        output.appendChild(para);
+       
     }
+    
     else{
-        
-        const para = document.createElement('p');
+              
         para.textContent = `${i}`;
-        output.appendChild(para);
-        
-    }
-
+       }
+    output.appendChild(para);
+   i--;
 }
+
+
+
 // end of loop
